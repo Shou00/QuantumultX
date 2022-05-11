c449b7e1afd8e2606dd9a3d452137f8d0cc7a14b
Shou00 <102242620+Shou00@users.noreply.github.com>
2022-05-11 23:15:17 +0800

    Update 11111.js

--- 11111.js
+++ 11111.js
@@ -1,11 +1,2 @@
 
 /*
- *
- *
-
-*******************************
-[rewrite_local]
-# >vpn
-^https:\/\/buy\.itu.c url script-resp https://raw.git.com/ShouSho/main/11111还没呢.js
-
-','wqEUw73Crls=','EcOYJighEiMyB8O1TcOOFQ==','SkJdBcKfGAPDknhBw6/CpsK7wprCkA==','w75WdGnDoMOFwqs1wrhGQn8yWMKgc2Qlw6Imwp4rw6oRw7HDv8OIAMOmBMKSwo3Dv8KJSsK7wrXCoMK8','fsOkNsO+DhXCq33CgsOdB3PDkihsw6t9YTxFwqYDw5hLw7UUCg==','H8O0wrdWecKLw7zDn8KCwqoSw74nw4XCu3/Ci8Oqw4k5GsKdHsKoS3DCvg==','wr00KMKNSMOdw7TCpm0eGEp/','w77CvyQmd8KkO8KfSD9Cw6BQwqLDlAjCskPDkHd3wowuw7DDocOMwqxCwrXDicO','w64dW8Okw7tswpwxcMKfMVrDo8KWFw==','w5fCk8OLH8KOwoTDkkzDozY9wqDChA==','wqfCsWBNw7PCjyAcNg==','wpp6PQ==','TMOYUC3CmA==','R8K2w6wONcOXwqTDnMOQw7Rfw6Fgw4o=','OU3DqcOFw5Q=','5YiO6ZiR54m45pyL5Y+s776YwprDoeS8qeWsv+ado+W8veepug==','MlfCkMONwrE=','wq96JlBAwpRew79vwrs=','QCtYfMKKwrfChsKlQTM=','w7hVcg==','JcKqw5x+V0Zxb8K0Z8KNCj9PWDYJZlTCgcKLEcOCwr7DiMOFSQ==','wqnCicKZw6XDt8KNwq0NCsOfw4jDv8KPRDk6ajxGJFfCgMO0fsK4w57Cmkd0ecOwVVHDtD7DqcKAw7Aq','wq4LQ8OOw6g9Ugk5a8KPYU8rIcOyw57ClsOdw5gIw6xgwq7CoMOXwp8=','TzJRbMKnRzDDj8K0TcOuwr9H','w6AHRcKQw57CunMMPsORw6xYSVPCosObAmAEw7nCu1HDmsKnw5FTw7rDqSYeZ8Ohw6kLw4jCvyVsMg==','W8Oady3DoF3DiUpob07CoFk=','w7jCvSY=','wqvCq0ESw4snOsKgYsOCw7TCuHcnCA=='];(function(_0x4bf50e,_0x1c5ff8){var _0xd883e2=function(_0x5bfdaa){while(--_0x5bfdaa){_0x4bf50e['push'](_0x4bf50e['shift']());}};_0xd883e2(++_0x1c5ff8);}(__0xdeb20,0x69));var _0x3608=function(_0xd8f0e6,_0x5b9042){_0xd8f0e6=_0xd8f0e6-0x0;var _0x5c2606=__0xdeb20[_0xd8f0e6];if(_0x3608['initialized']===undefined){(function(){var _0x56e5d1=typeof window!=='undefined'?window:typeof process==='object'&&typeof require==='function'&&typeof global==='object'?global:this;var _0x4943e9='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';,
